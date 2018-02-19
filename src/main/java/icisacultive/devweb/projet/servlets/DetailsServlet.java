a2bfafd3a6ac18a599e3feee8bf739e04b4dfb56
Yohann COLMAN <yohann.colman@hei.yncrea.fr>
2018-02-19 21:04:03 +0100

    Modification HTML redirection page détails (paramètre ajouté) - début d'ajout image particulière par article

---
+++ src/main/java/icisacultive/devweb/projet/servlets/DetailsServlet.java
@@ -0,0 +1,24 @@
+package icisacultive.devweb.projet.servlets;
+
+import icisacultive.devweb.projet.entities.Panier;
+import icisacultive.devweb.projet.managers.PanierLibrary;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/details")
+public class DetailsServlet extends GenericServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        TemplateEngine templateEngine = createTemplateEngine(req.getServletContext());
+        templateEngine.process("product_details", context, resp.getWriter());
+    }
+}
