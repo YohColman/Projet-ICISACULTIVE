09ca17302548c3b55b241fa296fe5db3c0d4a8a8
NTatou <noufele.tatou@hei.yncrea.fr>
2018-03-19 11:35:38 +0100

    Ajout CommandeDaoImpl

---
+++ src/main/java/icisacultive/devweb/projet/dao/impl/CommandeDaoImpl.java
@@ -0,0 +1,53 @@
+package icisacultive.devweb.projet.dao.impl;
+
+
+import icisacultive.devweb.projet.dao.CommandeDao;
+import icisacultive.devweb.projet.entities.Commande;
+
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommandeDaoImpl implements CommandeDao{
+
+    @Override
+    public List<Commande> listCommandes() {
+
+        String query = "SELECT commande.idcommande, commande.montant, commande.paye, commande.date, utilisateur.nom, utilisateur.prenom FROM commande INNER JOIN utilisateur ON commande.idutilisateur = utilisateur.idutilisateur ";
+        List<Commande> listOfCommandes = new ArrayList<>();
+        try (
+                Connection connection = DataSourceProvider.getDataSource().getConnection();
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(query);
+        ) {
+            while (resultSet.next()) {
+                listOfCommandes.add(new Commande(resultSet.getInt("idcommande"), resultSet.getInt("montant"), resultSet.getInt("paye"), resultSet.getString("nom"), resultSet.getString("prenom")));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return listOfCommandes;
+
+    }
+
+    @Override
+    public void paymentDone(String idCommande) {
+
+        String query = "UPDATE commande SET paye='1' WHERE idcommande=?;";
+        try {
+            Connection connection = DataSourceProvider.getDataSource().getConnection();
+            java.sql.PreparedStatement stmt = connection.prepareStatement(query);
+            stmt.setInt(1, idCommande);
+            ResultSet resultSet = stmt.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
